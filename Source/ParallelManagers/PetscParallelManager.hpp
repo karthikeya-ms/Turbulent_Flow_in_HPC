5f9e6e2515fceedd3a1dddef7a180fc744f9d276
karthikeya-ms <writetokarthikeya99@gmail.com>
2023-12-02 15:19:24 +0100

    PetscParallelManager header file implemented

---
+++ Source/ParallelManagers/PetscParallelManager.hpp
@@ -0,0 +1,55 @@
+/**
+ * Code obtained from iterators.cpph for reference
+ * ParallelBoundaryIterator(
+    FlowFieldType&                            flowField,
+    const Parameters&                         parameters,
+    Stencils::BoundaryStencil<FlowFieldType>& stencil,
+    int                                       lowOffset  = 0,
+    int                                       highOffset = 0
+  );
+*/
+
+#include "Definitions.hpp"
+#include "Parameters.hpp"
+#include "Stencils/PressureBufferFillStencil.hpp"
+#include "Stencils/PressureBufferReadStencil.hpp"
+#include "Stencils/VelocityBufferFillStencil.hpp"
+#include "Stencils/VelocityBufferReadStencil.hpp"
+#include "Iterators.hpp"
+#include "FlowField.hpp"
+
+namespace ParallelManagers {
+    class PetscParallelManager{
+    
+    protected: 
+
+        Parameters& parameters_;
+        FlowField& flowfield_;
+        Stencils::VelocityBufferFillStencil fillVelocityStencil;
+        Stencils::VelocityBufferReadStencil readVelocityStencil;
+        Stencils::PressureBufferFillStencil fillPressureStencil;
+        Stencils::PressureBufferReadStencil readPressureStencil;
+
+        ParallelBoundaryIterator<FlowField> velocityfillIterator;
+        ParallelBoundaryIterator<FlowField> velocityreadIterator;
+        ParallelBoundaryIterator<FlowField> pressurefillIterator;
+        ParallelBoundaryIterator<FlowField> pressurereadIterator;
+
+    public: 
+
+        PetscParallelManager(Parameters& parameters, FlowField& flowfield);
+
+        void communicateVelocity();
+        void communicatePressure();
+
+        virtual ~PetscParallelManager()= default;
+
+
+
+    };
+
+}
+
+
+
+
