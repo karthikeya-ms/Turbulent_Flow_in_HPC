7c0e5b00aad5f0fdca3b9da28f690dd1b261f90f
karthikeya-ms <writetokarthikeya99@gmail.com>
2023-12-03 14:45:15 +0100

    MPI_SendRecv implementation for PressureBuffers

--- Source/ParallelManagers/PetscParallelManager.cpp
+++ Source/ParallelManagers/PetscParallelManager.cpp
@@ -1,35 +1,200 @@
-#include "StdAfx.hpp"
-
 #include "PetscParallelManager.hpp"
 
-ParallelManagers::PetscParallelManager::PetscParallelManager(
-    Parameters &parameters, FlowField &flowField, int lowOffset = 0,
-    int highOffset = 0)
-    : parameters_(parameters), flowField_(flowField), lowOffset_(lowOffset),
-      highOffset_(highOffset) {}
+#include <mpi.h>
+#include <petsclog.h>
 
-ParallelManagers::PetscParallelManager::~PetscParallelManager() = default;
+#include "FlowField.hpp"
+#include "Parameters.hpp"
+
+ParallelManagers::PetscParallelManager::PetscParallelManager(Parameters& parameters, FlowField& flowfield):
+  parameters_(parameters),
+  flowfield_(flowfield),
+  fillVelocityStencil(parameters),
+  readVelocityStencil(parameters),
+  velocityfillIterator(flowfield, parameters, fillVelocityStencil, 0, 0),
+  velocityreadIterator(flowfield, parameters, readVelocityStencil, 0, 0),
+  fillPressureStencil(parameters),
+  readPressureStencil(parameters),
+  pressurefillIterator(flowfield, parameters, fillPressureStencil, 0, 0),
+  pressurereadIterator(flowfield, parameters, readPressureStencil, 0, 0) {}
 
 void ParallelManagers::PetscParallelManager::communicatePressure() {
-  auto fillStencil = Stencils::PressureBufferFillStencil(parameters_);
-  auto fillIterator = ParallelBoundaryIterator<FlowField>(
-      flowField_, parameters_, fillStencil, lowOffset_, highOffset_);
-  auto readStencil = Stencils::PressureBufferReadStencil(parameters_);
-  auto readIterator = ParallelBoundaryIterator<FlowField>(
-      flowField_, parameters_, readStencil, lowOffset_, highOffset_);
-  fillIterator.iterate();
-  MPI_Status* status;
-  MPI_Sendrecv(fillStencil.topPressureFillBuffer, *fillStencil.localSize, MY_MPI_FLOAT, parameters_.parallel.topNb,0, readStencil.topPressureReadBuffer, *readStencil.localSize, MY_MPI_FLOAT, parameters_.parallel.rank, 0, MPI_COMM_WORLD, status);
-  readIterator.iterate();
-}
 
-void ParallelManagers::PetscParallelManager::communicateVelocities() {
-  auto fillStencil = Stencils::VelocityBufferFillStencil(parameters_);
-  auto fillIterator = ParallelBoundaryIterator<FlowField>(
-      flowField_, parameters_, fillStencil, lowOffset_, highOffset_);
-  auto readStencil = Stencils::VelocityBufferReadStencil(parameters_);
-  auto readIterator = ParallelBoundaryIterator<FlowField>(
-      flowField_, parameters_, readStencil, lowOffset_, highOffset_);
-  fillIterator.iterate();
-  readIterator.iterate();
-}
+  pressurefillIterator.iterate();
+
+  if (parameters_.geometry.dim == 3) {
+    const int*  localSize = fillPressureStencil.localSize;
+
+    /* MPI_Sendrecv( 
+      const void* sendbuf , 
+      MPI_Count sendcount , 
+      MPI_Datatype sendtype , 
+      int dest , 
+      int sendtag , 
+      void* recvbuf , 
+      MPI_Count recvcount , 
+      MPI_Datatype recvtype , 
+      int source , 
+      int recvtag , 
+      MPI_Comm comm , 
+      MPI_Status* status);
+    */
+
+    MPI_Sendrecv( 
+      //LEFT TO RIGHT
+      fillPressureStencil.leftPressureFillBuffer.get(), 
+      localSize[1] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.leftNb, 
+      201, 
+      readPressureStencil.rightPressureReadBuffer.get(), 
+      localSize[1]  localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.rightNb, 
+      201, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+
+    MPI_Sendrecv( 
+      //RIGHT TO LEFT
+      fillPressureStencil.rightPressureFillBuffer.get(), 
+      localSize[1] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.rightNb, 
+      202, 
+      readPressureStencil.leftPressureReadBuffer.get(), 
+      localSize[1]  localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.leftNb, 
+      202, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+
+    MPI_Sendrecv( 
+      //TOP TO BOTTOM
+      fillPressureStencil.topPressureFillBuffer.get(), 
+      localSize[0] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.topNb, 
+      203, 
+      readPressureStencil.bottomPressureReadBuffer.get(), 
+      localSize[0] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.bottomNb, 
+      203, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+
+    MPI_Sendrecv( 
+      //BOTTOM TO TOP
+      fillPressureStencil.bottomPressureFillBuffer.get(), 
+      localSize[0] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.bottomNb, 
+      204, 
+      readPressureStencil.topPressureReadBuffer.get(), 
+      localSize[0] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.topNb, 
+      204, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+
+    MPI_Sendrecv( 
+      //FRONT TO BACK
+      fillPressureStencil.frontPressureFillBuffer.get(), 
+      localSize[0] * localSize[1], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.frontNb, 
+      205, 
+      readPressureStencil.backPressureReadBuffer.get(), 
+      localSize[0] * localSize[1], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.backNb, 
+      205, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+
+    MPI_Sendrecv( 
+      //BACK TO FRONT
+      fillPressureStencil.backPressureFillBuffer.get(), 
+      localSize[0] * localSize[1], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.backNb, 
+      206, 
+      readPressureStencil.frontPressureReadBuffer.get(), 
+      localSize[0] * localSize[1], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.frontNb, 
+      206, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+    
+  }
+
+  if(parameters_.geometry.dim = 2){
+
+    cont int* localSize = fillPressureStencil.localSize;
+
+    MPI_Sendrecv( 
+      //LEFT TO RIGHT
+      fillPressureStencil.leftPressureFillBuffer.get(), 
+      localSize[1] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.leftNb, 
+      201, 
+      readPressureStencil.rightPressureReadBuffer.get(), 
+      localSize[1]  localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.rightNb, 
+      201, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+
+    MPI_Sendrecv( 
+      //RIGHT TO LEFT
+      fillPressureStencil.rightPressureFillBuffer.get(), 
+      localSize[1] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.rightNb, 
+      202, 
+      readPressureStencil.leftPressureReadBuffer.get(), 
+      localSize[1]  localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.leftNb, 
+      202, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+
+    MPI_Sendrecv( 
+      //TOP TO BOTTOM
+      fillPressureStencil.topPressureFillBuffer.get(), 
+      localSize[0] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.topNb, 
+      203, 
+      readPressureStencil.bottomPressureReadBuffer.get(), 
+      localSize[0] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.bottomNb, 
+      203, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+
+    MPI_Sendrecv( 
+      //BOTTOM TO TOP
+      fillPressureStencil.bottomPressureFillBuffer.get(), 
+      localSize[0] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.bottomNb, 
+      204, 
+      readPressureStencil.topPressureReadBuffer.get(), 
+      localSize[0] * localSize[2], 
+      MY_MPI_FLOAT, 
+      parameters_.parallel.topNb, 
+      204, 
+      PETSC_COMM_WORLD, 
+      MPI_STATUS_IGNORE);
+  }
+  pressurereadIterator.iterate();
+}  
